4eda5418360de9af9c67ecce4d0390a43266f895
li.chengzhe <li_cheng_zhe@163.com>
2017-05-16 23:29:10 +0800

    add deal method,delete Test.java

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Sudoku
 Simple Sudoku game generation and answer using the basic syntax as much as possible.
+
+## For Example
+Load Soduku from sudoku.ini
+
+### Init State
+ 7   *   *   *   *   *   *   1   *  
+ *   *   *   *   3   *   *   5   *  
+ *   6   2   *   *   9   *   *   *  
+ 8   2   *   6   *   *   4   *   *  
+ 5   7   9   4   8   *   1   *   6  
+ *   3   *   2   5   *   *   *   *  
+ 3   9   4   7   2   5   8   *   1  
+ *   8   7   3   1   6   *   4   9  
+ 6   *   5   9   4   *   2   *   3  
+
+### Final State
+ 7   5   3   8   6   4   9   1   2  
+ 9   4   8   1   3   2   6   5   7  
+ 1   6   2   5   7   9   3   8   4  
+ 8   2   1   6   9   7   4   3   5  
+ 5   7   9   4   8   3   1   2   6  
+ 4   3   6   2   5   1   7   9   8  
+ 3   9   4   7   2   5   8   6   1  
+ 2   8   7   3   1   6   5   4   9  
+ 6   1   5   9   4   8   2   7   3 
+ 
--- Sudoku.java
+++ Sudoku.java
@@ -7,38 +7,90 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 /**
- * @author laputa 暂 不考虑性能问题及数独方阵出错的可能,考虑数独唯一解的情况
+ * @author laputa 前提：数独具有唯一解
  */
 public class Sudoku {
     
-    private final String FILE_NAME = "H:\\Code\\spaceGit\\Study\\src\\com\\laptop\\sudoku\\sudoku.ini";
-    
+    /**
+     * 数独可能出现数值的集合，'0'代表空
+     */
     private final int[] SUDOKU_ITEM = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
     
-    // 数独方阵
+    /**
+     * 计算数独花费的次数
+     */
+    private int dealTimes = 0;
+    
+    /**
+     * 数独矩阵
+     */
     private int[][] matrix = null;
     
-    // 方阵位置的可选集合
+    /**
+     * 数独矩阵每一位置可选值得集合
+     */
     private int[][][] mtxOptSet = null;
     
-    public Sudoku() {
+    /**
+     * 通过一个文件初始化数组矩阵
+     * 
+     * @param filePath
+     */
+    public Sudoku(String filePath) {
         super();
         this.matrix = new int[9][9];
         this.mtxOptSet = new int[9][9][10];// 第十个位置存放可选集合的大小
-    }
-    
-    public int[][] getMatrix() {
-        return matrix;
+        this.setMatrix(filePath);
     }
     
     /**
-     * 待完成
+     * 解数独,统计求解过程中循环处理的次数
      * 
-     * @return
+     * @return 正常处理完成/由于循环次数太多被迫中止
      */
-    public boolean tryComplete() {
-        this.dealMtxOptSet();
-        return false;
+    public boolean deal() {
+        boolean isSuccess = true;
+        while (this.dealMtxOptSet() != 0) {
+            this.dealTimes++;
+            // 处理次数大于81,说明数独无解,不再处理
+            if (this.dealTimes > 81) {
+                isSuccess = false;
+                break;
+            }
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    if (1 == this.mtxOptSet[i][j][9]) {
+                        this.matrix[i][j] = this.arrSum(this.mtxOptSet[i][j]);
+                        this.mtxOptSet[i][j] = new int[10];
+                    }
+                }
+            }
+        }
+        return isSuccess;
+    }
+    
+    /**
+     * 验证解是否符合数独的规则
+     * 
+     * @return 符合/不符合
+     */
+    public boolean judge() {
+        boolean dealSuccess = true;
+        outer: for (int i = -1; i < 9; i++) {
+            for (int j = -1; j < 9; j++) {
+                if (-1 == i && -1 == j) {
+                    break;
+                }
+                int[] arrTest = this.arrSort(this.mtx2Arr(i, j));
+                for (int k = 0; k < 9; k++) {
+                    if (k + 1 != arrTest[k]) {
+                        dealSuccess = false;
+                        break outer;
+                    }
+                }
+            }
+        }
+        return dealSuccess;
     }
     
     /**
@@ -46,9 +98,21 @@ public class Sudoku {
      *            行号：0-8 -1代表不取行，只取对应列
      * @param column
      *            列号：0-8 -1代表不取列，只取对应行
-     * @return
+     * @return 可选值的集合,'0'代表空
      */
     private int[] getOptSetItem(int line, int column) {
+        int[] temp = mtx2Arr(line, column);
+        return this.decArrSet(this.SUDOKU_ITEM, temp);
+    }
+    
+    /**
+     * @param line
+     *            行号：0-8 -1代表不取行，只取对应列
+     * @param column
+     *            列号：0-8 -1代表不取列，只取对应行
+     * @return 数独中需要满足为[1-9]的区域值的集合
+     */
+    private int[] mtx2Arr(int line, int column) {
         int[] temp = new int[9];// 自动初始化为0!
         if (-1 == line) {
             for (int i = 0; i < 9; i++) {
@@ -60,42 +124,50 @@ public class Sudoku {
             }
         } else {
             int count = 0;
-            for (int i = line; i < line + 3; i++) {
-                for (int j = column; j < column + 3; j++) {
+            int blockI = line - line % 3;
+            int blockJ = column - column % 3;
+            // 元素所在的3*3矩阵转Array
+            for (int i = blockI; i < blockI + 3; i++) {
+                for (int j = blockJ; j < blockJ + 3; j++) {
                     temp[count++] = this.matrix[i][j];
                 }
             }
         }
-        return this.decArrSet(this.SUDOKU_ITEM, temp);
+        return temp;
     }
     
     /**
-     * 对每一个方阵中为空的值计算可选集合
+     * 对每一个矩阵中为空的值计算可选集合
      * 
      * @return 一个可选集合的二维数组
      */
-    private void dealMtxOptSet() {
+    private int dealMtxOptSet() {
+        int maxOptSet = 0;
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (0 == this.matrix[i][j]) {
                     int[] lineSet = this.getOptSetItem(i, -1);
                     int[] columnSet = this.getOptSetItem(-1, j);
-                    int[] squareSet = this.getOptSetItem(i - i % 3, j - j % 3);
+                    int[] squareSet = this.getOptSetItem(i, j);
                     this.mtxOptSet[i][j] = this.addArrSet(this.addArrSet(lineSet, columnSet), squareSet);
                     for (int p = 0; p < 9; p++) {
                         if (0 != this.mtxOptSet[i][j][p]) {
                             this.mtxOptSet[i][j][9]++;
                         }
+                        if (mtxOptSet[i][j][9] > maxOptSet) {
+                            maxOptSet = mtxOptSet[i][j][9];
+                        }
                     }
                 }
             }
         }
+        return maxOptSet;
     }
     
     /**
-     * 从文件读入方阵,存于成员变量:文件为9*9方阵,只出现字符[0-9],[0]代表方阵中对应位置为空
+     * 从文件读入矩阵,存于成员变量:文件为9*9矩阵,只出现字符[0-9],[0]代表矩阵中对应位置为空
      */
-    public boolean setMatrix() {
+    private boolean setMatrix(String fileName) {
         int i = 0;
         int j = 0;
         int total = 0;
@@ -103,7 +175,7 @@ public class Sudoku {
         boolean isSuccess = true;
         InputStreamReader reader = null;
         try {
-            InputStream in = new FileInputStream(new File(FILE_NAME));
+            InputStream in = new FileInputStream(new File(fileName));
             reader = new InputStreamReader(in);
             while ((item = reader.read()) != -1) {
                 if (this.isRightItem(item)) {
@@ -146,13 +218,19 @@ public class Sudoku {
     /**
      * 控制台展示
      */
-    public void showMatrix() {
+    public void show() {
+        System.out.println("deal times:" + this.dealTimes);
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (0 == matrix[i][j]) {
-                    System.out.printf("%-6s", "[" + this.mtxOptSet[i][j][9] + "]");
+                    int optNum = this.mtxOptSet[i][j][9];
+                    if (0 != optNum) {
+                        System.out.printf("%-4s", "[" + this.mtxOptSet[i][j][9] + "]");
+                    } else {
+                        System.out.printf("%-4s", " * ");
+                    }
                 } else {
-                    System.out.printf("%-6s", matrix[i][j] + " ");
+                    System.out.printf("%-4s", " " + matrix[i][j] + " ");
                 }
             }
             System.out.println();
@@ -199,5 +277,40 @@ public class Sudoku {
             }
         }
         return arrResult;
+    }
+    
+    /**
+     * 数组含9个int值
+     * 
+     * @param arr
+     *            数组
+     * @return 数组的和
+     */
+    private int arrSum(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+    
+    /**
+     * 数组含9个int值
+     * 
+     * @param arr
+     *            初始数组
+     * @return 从小到大排序好的数组
+     */
+    private int[] arrSort(int[] arr) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = i; j < 9; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+        return arr;
     }
 }
